258229f9841f88c153ec5c6a9ec0c872536c6e0e
Alexander.Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2013-11-12 23:21:38 +0400

    Removed IDEA part from repository.

--- SBT/idea-plugin/src/main/scala/org/jetbrains/sbt/project/settings/SbtSettingsListener.scala
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.sbt
-package project.settings
-
-import com.intellij.openapi.externalSystem.settings.ExternalSystemSettingsListener
-
-/**
- * @author Pavel Fatin
- */
-trait SbtSettingsListener extends ExternalSystemSettingsListener[SbtProjectSettings]
---
+++ build.sbt
@@ -0,0 +1,9 @@
+sbtPlugin := true
+
+name := "sbt-structure"
+
+organization := "org.jetbrains"
+
+version := "1.0"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.2" % "test"
