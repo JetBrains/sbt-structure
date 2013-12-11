07172713f1f8bddbd731b7cf500a9905db751a99
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-12-11 14:10:00 +0400

    Extract SBT version (v1.0.4)

--- src/main/scala/org/jetbrains/sbt/Extractor.scala
+++ src/main/scala/org/jetbrains/sbt/Extractor.scala
@@ -15,6 +15,8 @@ object Extractor {
   def extractStructure(state: State, download: Boolean): StructureData = {
     val structure = Project.extract(state).structure
 
+    val sbtVersion = Keys.sbtVersion.get(structure.data).get
+
     val scalaData = extractScala(state)
 
     val allProjectRefs = structure.allProjectRefs
@@ -26,7 +28,7 @@ object Extractor {
       RepositoryData(modulesData)
     }
 
-    StructureData(scalaData, projectsData.sortBy(_.base), repositoryData)
+    StructureData(sbtVersion, scalaData, projectsData.sortBy(_.base), repositoryData)
   }
 
   def extractScala(state: State): ScalaData = {
