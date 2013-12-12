419332e967c981fd519aa0fa68207a3e282d6d01
Pavel Fatin <pavel.fatin@jetbrains.com>
2013-12-12 16:38:20 +0400

    JavaData is optional

--- src/main/scala/org/jetbrains/sbt/Extractor.scala
+++ src/main/scala/org/jetbrains/sbt/Extractor.scala
@@ -60,7 +60,7 @@ object Extractor {
         case _ => Seq.empty
       }
 
-      JavaData(home, options)
+      if (home.isDefined || options.nonEmpty) Some(JavaData(home, options)) else None
     }
 
     val scala: Option[ScalaData] = {
