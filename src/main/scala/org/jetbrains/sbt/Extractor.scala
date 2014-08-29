5ecb64076011a17e581c367ddf90d3c592099574
Nick Obedin <dancingrobot84@gmail.com>
2014-08-29 16:58:32 +0400

    + ivy cache path extraction

--- src/main/scala/org/jetbrains/sbt/Extractor.scala
+++ src/main/scala/org/jetbrains/sbt/Extractor.scala
@@ -30,7 +30,9 @@ object Extractor {
       RepositoryData(modulesData)
     }
 
-    StructureData(sbtVersion, scalaData, projectsData, repositoryData)
+    val localCachePath = Option(System.getProperty("sbt.ivy.home", System.getProperty("ivy.home")))
+
+    StructureData(sbtVersion, scalaData, projectsData, repositoryData, localCachePath)
   }
 
   def extractScala(state: State): ScalaData = {
