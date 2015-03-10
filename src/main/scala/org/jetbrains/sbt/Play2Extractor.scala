300597dd60cf407e77767b73e6a6cac8da86bf92
dmitry.naydanov <dmitry.naydanov@jetbrains.com>
2015-03-10 18:30:50 +0300

    Play2: alias setting keys (for #SCL-8215 and similar problems caused by changed Sbt keys names)

--- src/main/scala/org/jetbrains/sbt/Play2Extractor.scala
+++ src/main/scala/org/jetbrains/sbt/Play2Extractor.scala
@@ -17,6 +17,7 @@ object Play2Extractor {
   private val PLAY_VERSION = "playVersion"
   //default
   private val TEMPLATES_IMPORT = "twirlTemplatesImports"
+  private val TEMPLATE_IMPORT_ALIAS = "twirlTemplateImports"
   private val ROUTES_IMPORT = "playRoutesImports"
   //options
   private val TEST_OPTIONS = "testOptions"
@@ -49,8 +50,8 @@ object Play2Extractor {
     }
   }
 
-  private class KeyChain(val markerKey: KeyWithScope, val keys: Seq[KeyWithScope]) {
-    protected val allKeys = markerKey +: keys
+  private class KeyChain(val markerKey: KeyWithScope, val keys: Seq[KeyWithScope], val aliasKeys: Seq[AliasKey] = Seq.empty) {
+    protected val allKeys = (markerKey +: keys) ++ aliasKeys
 
     def processKey(key: ScopedKey[_]): Unit = {
       allKeys.find(_.extract(key))
@@ -76,7 +77,7 @@ object Play2Extractor {
           case pName =>
             SettingKey(attrKey).in(projectRef, Compile) get structureData flatMap transform exists {
               case vv =>
-                myValues.put(pName, vv)
+                saveValues(pName, vv) //myValues.put(pName, vv)
                 true
             }
         }
@@ -85,9 +86,20 @@ object Play2Extractor {
 
     def transform(any: Any): Option[Value]
 
+    def saveValues(key: String, vv: Value) = myValues.put(key, vv)
+
     def toKeyInfo(projectNames: Set[String]) = KeyInfo(label, label, myValues.toSeq.filter{case (p, _) => projectNames.contains(p)})
 
     def toKeyInfo: KeyInfo[Value] = KeyInfo(label, label, myValues.toSeq)
+  }
+
+  private class AliasKey(label: String, val delegate: KeyWithScope)(implicit data: (ProjectRef, Settings[Scope]))
+    extends KeyWithScope(label, data._1, data._2) {
+    override type Value = delegate.Value
+
+    override def saveValues(key: String, vv: Value) = delegate.saveValues(key, vv)
+
+    override def transform(any: Any): Option[Value] = delegate.transform(any)
   }
 
   private class StringKey(label: String)(implicit data: (ProjectRef, Settings[Scope]))
@@ -171,7 +183,16 @@ class Play2Extractor(structure: sbt.Load.BuildStructure, projectRef: ProjectRef,
   private val PLAY_VERSION_KEY = new StringKey(PLAY_VERSION)
   private val TEST_OPTIONS_KEY = new StringKey(TEST_OPTIONS)
   //imports keys
-  private val TEMPLATES_IMPORT_KEY = new SeqStringKey(TEMPLATES_IMPORT)
+  private val TEMPLATES_IMPORT_KEY = new SeqStringKey(TEMPLATES_IMPORT) {
+    override def transform(any: Any): Option[Value] = {
+      super.transform(any).map (_.map {
+        case "views.%format%._" => "views.xml._"
+        case a => a
+       }
+      )
+    }
+  }
+  private val TEMPLATE_IMPORT_KEY_ALIAS = new AliasKey(TEMPLATE_IMPORT_ALIAS, TEMPLATES_IMPORT_KEY)
   private val ROUTES_IMPORT_KEY = new SeqStringKey(ROUTES_IMPORT)
   //dirs keys
   private val PLAY_CONF_DIR_KEY = new FileKey(PLAY_CONF_DIR)
@@ -180,8 +201,9 @@ class Play2Extractor(structure: sbt.Load.BuildStructure, projectRef: ProjectRef,
   private val ALL_USUAL_KEYS = Seq(PLAY_VERSION_KEY, TEST_OPTIONS_KEY, TEMPLATES_IMPORT_KEY,
     ROUTES_IMPORT_KEY, PLAY_CONF_DIR_KEY, SOURCE_DIR_KEY)
 
+  private val ALL_ALIAS_KEYS = Seq(TEMPLATE_IMPORT_KEY_ALIAS)
 
-  private val chain = new KeyChain(PLAY_PLUGIN_KEY, ALL_USUAL_KEYS)
+  private val chain = new KeyChain(PLAY_PLUGIN_KEY, ALL_USUAL_KEYS, ALL_ALIAS_KEYS)
 
   def extract(): Option[Play2Data] = {
     val keys = state.attributes.get(sessionSettings) match {
