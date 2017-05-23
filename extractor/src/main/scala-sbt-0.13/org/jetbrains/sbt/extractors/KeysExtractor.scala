ef321f8b26393552f6e98900aad493d862fafdcb
Justin Kaeser <justin.kaeser@jetbrains.com>
2017-05-23 16:27:04 +0200

    guard against NPEs and other errors in sbt API during setting import. #SCL-12028 in progress filter for tasks instead of settings in taskData. #SCL-12029 in progress

--- extractor/src/main/scala-sbt-0.13/org/jetbrains/sbt/extractors/KeysExtractor.scala
+++ extractor/src/main/scala-sbt-0.13/org/jetbrains/sbt/extractors/KeysExtractor.scala
@@ -3,7 +3,7 @@ package org.jetbrains.sbt.extractors
 import org.jetbrains.sbt.StructureKeys
 import org.jetbrains.sbt.structure.{CommandData, SettingData, TaskData}
 import sbt.jetbrains.BadCitizen
-import sbt.{AttributeKey, BuiltinCommands, Def, Extracted, KeyRanks, Keys, Project, SettingKey, Task}
+import sbt.{AttributeKey, BuiltinCommands, Def, Extracted, KeyRanks, Keys, Logger, Project, SettingKey, Task}
 
 import scala.util.control.NonFatal
 
@@ -25,7 +25,7 @@ object KeysExtractor {
     } catch {
       // prior to sbt 0.13.9, this error is not caught, so mitigate it here in case users are affected
       case NonFatal(x) =>
-        Keys.state.value.log.warn(s"Unable to extract task and setting keys. Upgrade your project sbt to 0.13.11 or later. Error was: ${x.getMessage}")
+        Keys.state.value.log.warn(s"Unable to import task and setting keys. Upgrade your project sbt to 0.13.11 or later. Error was: ${x.getMessage}")
         Seq.empty
     }
   )
@@ -34,33 +34,26 @@ object KeysExtractor {
     val state = Keys.state.value
     val extracted = Project.extract(state)
 
-    for {
-      key <- StructureKeys.allKeys.value
-      if ! BuiltinCommands.isTask(key.manifest)
-    } yield {
-      val displayString = settingStringValue(extracted,key)
-      SettingData(key.label, key.description, key.rank, displayString)
+    guarded(Keys.state.value.log, Seq.empty[SettingData]) {
+      for {
+        key <- StructureKeys.allKeys.value
+        if key != null && ! BuiltinCommands.isTask(key.manifest)
+      } yield {
+        val displayString = settingStringValue(extracted,key)
+        SettingData(key.label, key.description, key.rank, displayString)
+      }
     }
   }
 
-  private def settingStringValue(extracted: Extracted, key: AttributeKey[_]) = for {
-    value <- extracted.getOpt(SettingKey(key))
-    // only get a display string if it has a chance of being meaningful to the user, ie is redefined
-    if value != null && (value.getClass.getMethod("toString").getDeclaringClass ne classOf[Any])
-    stringValue <- Option(value.toString) // some zany settings might return a null toString
-  } yield {
-    val trimmed = stringValue.toString.trim
-    if (trimmed.length > maxValueStringLength)
-      trimmed.substring(0, maxValueStringLength - 3) + "..."
-    else trimmed
-  }
-
   val taskData: Def.Initialize[Task[Seq[TaskData]]] = Def.task {
-    for {
-      key <- StructureKeys.allKeys.value
-      if !BuiltinCommands.isTask(key.manifest)
-    } yield
-      TaskData(key.label, key.description, key.rank)
+
+    guarded(Keys.state.value.log, Seq.empty[TaskData]) {
+      for {
+        key <- StructureKeys.allKeys.value
+        if key != null && BuiltinCommands.isTask(key.manifest)
+      } yield
+        TaskData(key.label, key.description, key.rank)
+    }
   }
 
 
@@ -75,6 +68,30 @@ object KeysExtractor {
       command <- commands
       name <- BadCitizen.commandName(command)
     } yield CommandData(name, command.help(state).brief)
+  }
+
+
+  private def settingStringValue(extracted: Extracted, key: AttributeKey[_]) = for {
+    value <- extracted.getOpt(SettingKey(key))
+    // only get a display string if it has a chance of being meaningful to the user, ie is redefined
+    if value != null && (value.getClass.getMethod("toString").getDeclaringClass ne classOf[Any])
+    stringValue <- Option(value.toString) // some zany settings might return a null toString
+  } yield {
+    val trimmed = stringValue.toString.trim
+    if (trimmed.length > maxValueStringLength)
+      trimmed.substring(0, maxValueStringLength - 3) + "..."
+    else trimmed
+  }
+
+  private def guarded[T](log: Logger, default: =>T)(f: =>T): T = {
+    try {f}
+    catch {
+      case NonFatal(x) =>
+        log.warn(
+          s"Unable to import some setting or task data. Is your project configured correctly?\n" +
+            s"Error was: ${x.getMessage}\n${x.getStackTraceString}")
+        default
+    }
   }
 
 }
