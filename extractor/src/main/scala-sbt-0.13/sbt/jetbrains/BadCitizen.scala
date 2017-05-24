88035e73a4360abe0b9b6b20c166f44e64cf5c1c
Justin Kaeser <justin.kaeser@jetbrains.com>
2017-05-24 11:48:49 +0200

    more guards against API changes

--- extractor/src/main/scala-sbt-0.13/sbt/jetbrains/BadCitizen.scala
+++ extractor/src/main/scala-sbt-0.13/sbt/jetbrains/BadCitizen.scala
@@ -2,15 +2,22 @@ package sbt.jetbrains
 
 import sbt.{Command, SimpleCommand}
 
+import scala.util.control.NonFatal
+
 /**
   * The bad citizen accesses private[sbt] classes
   */
 object BadCitizen {
 
   def commandName(cmd: Command): Option[String] =
-    cmd match {
-      case simple: SimpleCommand => Option(simple.name)
-      case _ => None
+    try {
+      cmd match {
+        case simple: SimpleCommand => Option(simple.name)
+        case _ => None
+      }
+    } catch {
+      // guard against internal API changes
+      case NonFatal(x) => None
     }
 
 }
