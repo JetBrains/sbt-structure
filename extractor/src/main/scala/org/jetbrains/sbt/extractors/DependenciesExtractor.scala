97bb16c33014e17d35a7f3de1da4a56d7e68b241
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2023-10-17 19:33:09 +0200

    some changes in DependenciesExtractor.scala

--- extractor/src/main/scala/org/jetbrains/sbt/extractors/DependenciesExtractor.scala
+++ extractor/src/main/scala/org/jetbrains/sbt/extractors/DependenciesExtractor.scala
@@ -1,12 +1,14 @@
 package org.jetbrains.sbt
 package extractors
 
+import org.jetbrains.sbt.extractors.DependenciesExtractor.SbtConfigurationName
 import org.jetbrains.sbt.structure._
 import org.jetbrains.sbt.{structure => jb}
 import sbt._
 import sbt.jetbrains.apiAdapter._
 
 import scala.collection.Seq
+import scala.language.postfixOps
 
 /**
  * @author Nikolay Obedin
@@ -21,7 +23,7 @@ class DependenciesExtractor(projectRef: ProjectRef,
                             testConfigurations: Seq[sbt.Configuration],
                             sourceConfigurations: Seq[sbt.Configuration],
                             insertProjectTransitiveDependencies: Boolean,
-                            projectToConfigurations: Map[ProjectRef, Seq[jb.Configuration]])
+                            projectToConfigurations: Map[ProjectRef, Seq[SbtConfigurationName]])
   extends ModulesOps {
 
   private[extractors] def extract: DependencyData = {
@@ -33,14 +35,16 @@ class DependenciesExtractor(projectRef: ProjectRef,
   }
 
   private def transitiveProjectDependencies: Dependencies = {
+    val IDEAScopes = Seq(jb.Configuration.Compile, jb.Configuration.Runtime, jb.Configuration.Provided).map(_.name)
+    val sourceConfigurationsNames = sourceConfigurations.map(_.name)
+
     val dependencies = projectToConfigurations.map { case (project, configurations) =>
-      val IDEAScopes = Seq(jb.Configuration.Compile, jb.Configuration.Runtime, jb.Configuration.Provided).map(_.name)
-      val sourceConfigurationsNames = sourceConfigurations.map(_.name)
-      val transformedConfigurations = mapConfigurations(configurations).map { configuration =>
-        // additional mapping is required in case of situation where project1 dependsOn project2 in two custom configurations -> the test one
-        // and the one containing sources. Without this mapping custom test configuration will be transformed to TEST in #mapConfigurations and in the scala plugin, scope
+      val transformedConfigurations = mapSbtConfigurations(configurations).map { configuration =>
+        // additional mapping is required in case of situation
+        // where project1 dependsOn project2 in two custom configurations: the test one and the one containing sources.
+        // Without this mapping custom test configuration will be transformed to TEST in #mapConfigurations and in the scala plugin, scope
         // for this dependency would be TEST which would not be truth
-      if (!IDEAScopes.contains(configuration.name) && sourceConfigurationsNames.contains(configuration.name)) {
+        if (!IDEAScopes.contains(configuration.name) && sourceConfigurationsNames.contains(configuration.name)) {
           jb.Configuration.Compile
         } else {
           configuration
@@ -91,6 +95,9 @@ class DependenciesExtractor(projectRef: ProjectRef,
       .toSeq
   }
 
+  private def mapSbtConfigurations(configurations: Seq[SbtConfigurationName]): Seq[jb.Configuration] =
+    mapConfigurations(configurations.map(c => jb.Configuration(c.name)))
+
   // We have to perform this configurations mapping because we're using externalDependencyClasspath
   // rather than libraryDependencies (to acquire transitive dependencies),  so we detect
   // module presence (in external classpath) instead of explicitly declared configurations.
@@ -117,8 +124,11 @@ object DependenciesExtractor extends SbtStateOps with TaskOps {
     val state = Keys.state.value
     val projectRef = Keys.thisProjectRef.value
     val options = StructureKeys.sbtStructureOpts.value
+    //example: Seq(compile, runtime, test, provided, optional)
     val dependencyConfigurations = StructureKeys.dependencyConfigurations.value
+    //example: Seq(test, it)
     val testConfigurations = StructureKeys.testConfigurations.value
+    //example: Seq(compile, runtime)
     val sourceConfigurations = StructureKeys.sourceConfigurations.value
     val buildDependencies = Keys.buildDependencies.value
 
@@ -132,31 +142,32 @@ object DependenciesExtractor extends SbtStateOps with TaskOps {
         .map(throwExceptionIfUpdateFailed)
         .onlyIf(options.download)
 
-    val allSourceConfigurationsTask = StructureKeys.allSourceConfigurations.in(projectRef).get(state)
-    val allTestConfigurationsTask = StructureKeys.allTestConfigurations.in(projectRef).get(state)
+    val allSourceConfigurationsTask = StructureKeys.allSourceConfigurations.in(projectRef).get(state).map(_.distinct)
+    val allTestConfigurationsTask = StructureKeys.allTestConfigurations.in(projectRef).get(state).map(_.distinct)
 
-    val classpathConfigurationTask = sbt.Keys.classpathConfiguration.in(projectRef)
+    val classpathConfigurationTask: Task[Map[sbt.Configuration, sbt.Configuration]] = sbt.Keys.classpathConfiguration.in(projectRef)
       .forAllConfigurations(state, dependencyConfigurations)
 
-   val settings = Keys.settingsData.value
+    val settings = Keys.settingsData.value
 
     Def.task {
       (for {
         unmanagedClasspath <- unmanagedClasspathTask
         externalDependencyClasspathOpt <- externalDependencyClasspathTask
-        classpathConfiguration <- classpathConfigurationTask
+        classPathConfiguration <- classpathConfigurationTask
         allSourceConfigurations <- allSourceConfigurationsTask
         allTestConfigurations <- allTestConfigurationsTask
       } yield {
+        val allApplicableConfigurations = (allTestConfigurations ++ allSourceConfigurations).distinct.map(x => SbtConfigurationName(x.name))
+        val projectToTransitiveDependencies: Map[ProjectRef, Seq[SbtConfigurationName]] =
+          getTransitiveDependenciesForProject(
+            projectRef,
+            settings,
+            buildDependencies,
+            allApplicableConfigurations,
+            classPathConfiguration
+          )
 
-        val projectToTransitiveDependencies = getTransitiveDependenciesForProject(
-          allSourceConfigurations,
-          allTestConfigurations,
-          classpathConfiguration,
-          projectRef,
-          settings,
-          buildDependencies
-        )
         val extractor = new DependenciesExtractor(
           projectRef,
           buildDependencies,
@@ -173,64 +184,103 @@ object DependenciesExtractor extends SbtStateOps with TaskOps {
     }
   }
 
-  private case class Dependency(project: ProjectRef, configuration: jb.Configuration)
-
-
-  private def getTransitiveDependenciesForProject(
-    allSourceConfigurations: Seq[sbt.Configuration],
-    allTestConfigurations: Seq[sbt.Configuration],
-    classPathConfiguration: Map[sbt.Configuration, sbt.Configuration],
-    projectRef: ProjectRef,
-    settings: Settings[Scope],
-    buildDependencies: BuildDependencies
-  ): Map[ProjectRef, Seq[jb.Configuration]] = {
-
-    def retrieveAllTransitiveDependencies(config: sbt.Configuration): Seq[Dependency] =
-      Classpaths.interSort(projectRef, config, settings, buildDependencies)
-        .filter { case (dep, _) => dep != projectRef }
-        .map { case (projectRef, configName) =>
-          Dependency(projectRef, jb.Configuration(configName))
-        }
-
-    val configurationToDependencies = classPathConfiguration
-      .map { case (selfConfig, config) =>
-        (jb.Configuration(selfConfig.name), retrieveAllTransitiveDependencies(config))
-      }
-
-    val allApplicableConfigurations = (allTestConfigurations ++ allSourceConfigurations).distinct
-    val removedDependenciesWithoutSources = configurationToDependencies
-      .mapValues { removeDependenciesWithoutSources(_, allApplicableConfigurations) }
-
-    val projectToConfigurations = collection.mutable.Map.empty[ProjectRef, Seq[jb.Configuration]]
-    removedDependenciesWithoutSources.foreach { case (config, projects) =>
-      projects.foreach { project =>
-        projectToConfigurations(project) = projectToConfigurations.getOrElse(project, Seq.empty) :+ config
-      }
-    }
-
-    projectToConfigurations.toMap
+  /**
+   * Represents a single dependency item returned from [[sbt.Classpaths.interSort]]<br>
+   * Note that single dependency can be represented by multiple `ProjectDependency` instances<br>
+   * For example in this project {{{
+   *   val root = project.dependsOn(
+   *     proj1 % "compile -> test",
+   *     proj2 % "compile",
+   *   )
+   * }}}
+   * "proj1" dependency for "compile" scope will be represented by these 3 items {{{
+   *   Seq(ProjectDependency(proj1, compile), ProjectDependency(proj1, runtime), ProjectDependency(proj1, test))
+   * }}}
+   * "proj2" dependency for "compile" scope will be represented by these 1 item {{{
+   *   Seq(ProjectDependency(proj2, compile))
+   * }}}
+   */
+  private case class ProjectDependency(project: ProjectRef, configuration: SbtConfigurationName)
+  private object ProjectDependency {
+    def apply(tuple: (ProjectRef, String)): ProjectDependency =
+      ProjectDependency(tuple._1, SbtConfigurationName(tuple._2))
   }
 
   /**
-   * The aim of this method is to remove project dependencies with configurations that do not have sources e.g. provided.
-   * It needs to be done because `Classpaths#interSort` generate transitive dependencies by analyzing configurations strings (e.g. "compile->provided")
-   * So for such project: <br>
-   *   {{{ val root = (project in file(".")).dependsOn(proj1 % "compile->provided") }}}
-   * `Classpaths#interSort` will return:
-   * {{{ compile -> (proj1, "provided)
-   * runtime -> (proj1, "provided)
-   * test -> (proj1, "provided) }}}
-   * which in practise means that we shouldn't add `proj1` as a dependency to `root`
-  */
-  private def removeDependenciesWithoutSources(scopeToDependencies: Seq[Dependency], allApplicableConfigs: Seq[sbt.Configuration]): Seq[ProjectRef] = {
-    val groupedByProject = scopeToDependencies
-      .groupBy(_.project)
-      .mapValues(_.map(_.configuration.name))
+   * Identifies `name` field of [[sbt.librarymanagement.Configuration.name]]<br>
+   * This extra type is introduced to avoid untyped String usage for a more clear data flow
+   */
+  case class SbtConfigurationName(name: String)
 
-    val allApplicableConfigsNames = allApplicableConfigs.map(_.name)
-    groupedByProject.filter { case (_, configs) =>
-      configs.exists(allApplicableConfigsNames.contains)
-    }.keys.toSeq
+  private def getTransitiveDependenciesForProject(
+    projectRef: ProjectRef,
+    settings: Settings[Scope],
+    buildDependencies: BuildDependencies,
+    allApplicableConfigurations: Seq[SbtConfigurationName],
+    classPathConfiguration: Map[sbt.Configuration, sbt.Configuration]
+  ): Map[ProjectRef, Seq[SbtConfigurationName]] = {
+    val configToDependencies: Map[SbtConfigurationName, Seq[ProjectDependency]] =
+      classPathConfiguration.map { case (config, configInternal) =>
+        val projectDependencies = retrieveTransitiveProjectDependencies(projectRef, configInternal, settings, buildDependencies)
+        (SbtConfigurationName(config.name), projectDependencies)
+      }
+
+    val configToApplicableProjects: Map[SbtConfigurationName, Seq[ProjectRef]] =
+      configToDependencies.mapValues(findDependentProjectsWithApplicableConfig(_, allApplicableConfigurations))
+
+    invert(configToApplicableProjects)
+  }
+
+  //TODO: move to some utility class
+  private def invert[K, V](map: Map[K, Seq[V]]): Map[V, Seq[K]] = {
+    val tuples: Seq[(V, K)] = for {
+      (key, values) <- map.toSeq
+      value <- values
+    } yield value -> key
+    tuples.groupBy(_._1).mapValues(_.map(_._2))
+  }
+
+  private def retrieveTransitiveProjectDependencies(
+    projectRef: ProjectRef,
+    config: sbt.Configuration,
+    settings: Settings[Scope],
+    buildDependencies: BuildDependencies
+  ): Seq[ProjectDependency] = {
+    val dependenciesAll = Classpaths.interSort(projectRef, config, settings, buildDependencies)
+    val dependenciesWithoutSelf = dependenciesAll.filter(_._1 != projectRef)
+    dependenciesWithoutSelf.map(ProjectDependency.apply)
+  }
+
+  //TODO: improve this doc. add more examples (in addition to `proj1 % "compile->provided"` dependency add 1-2 more examples)
+  /**
+   * The goal of this method is to remove project dependencies with configurations that do not have sources e.g. provided.
+   * It needs to be done because `Classpaths#interSort` generate transitive dependencies by analyzing configurations strings (e.g. "compile->provided")
+   * So for such project: {{{
+   *   val root = project.dependsOn(
+   *     proj1 % "compile->provided"
+   *   )
+   * }}}
+   * `Classpaths#interSort` will return: {{{
+   *   compile -> (proj1, "provided)
+   *   runtime -> (proj1, "provided)
+   *   test -> (proj1, "provided)
+   * }}}
+   * which in practice means that we shouldn't add `proj1` as a dependency to `root`
+   */
+  private def findDependentProjectsWithApplicableConfig(
+    dependencies: Seq[ProjectDependency],
+    applicableConfigs: Seq[SbtConfigurationName]
+  ): Seq[ProjectRef] = {
+    val projectToConfigs: Map[ProjectRef, Seq[SbtConfigurationName]] = dependencies
+      .groupBy(_.project)
+      .mapValues(_.map(_.configuration))
+
+    val projectToConfigsWithAtLeastOneApplicableConfig =
+      projectToConfigs.filter { case (_, configs) =>
+        configs.exists(applicableConfigs.contains)
+      }
+
+    projectToConfigsWithAtLeastOneApplicableConfig.keys.toSeq
   }
 
   private def throwExceptionIfUpdateFailed(result: Result[Map[sbt.Configuration,Keys.Classpath]]): Map[sbt.Configuration, Keys.Classpath] =
