476ae4bb957f8d93e4fe71dcc802fbf0bec68632
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-01-17 14:30:26 +0100

    [managed sources] Add an sbt auto plugin for generating managed sources in all sbt subprojects #SCL-22636

---
+++ extractor/src/main/scala/org/jetbrains/sbt/GenerateAllManagedSourcesPlugin.scala
@@ -0,0 +1,27 @@
+package org.jetbrains.sbt
+
+import sbt.Keys.managedSources
+//noinspection scala2InSource3
+import sbt._
+
+/**
+ * An sbt plugin developed for the purposes of generating all `managedSources` in a given sbt project.
+ * The plugin is implemented as a global task which invokes the `managedSources` task in the Compile and Test
+ * configurations of all subprojects within the sbt project and aggregates the generated source files.
+ */
+object GenerateAllManagedSourcesPlugin extends AutoPlugin {
+  override def trigger = allRequirements
+
+  object autoImport {
+    val ideaGenerateAllManagedSources = taskKey[Seq[File]]("Generate managed sources in all subprojects")
+  }
+
+  import autoImport.ideaGenerateAllManagedSources
+
+  private val scopeFilter = ScopeFilter(inAnyProject, inConfigurations(Compile, Test))
+
+  //noinspection scala2InSource3
+  override lazy val globalSettings: Seq[Def.Setting[_]] = Seq(
+    ideaGenerateAllManagedSources := { managedSources.all(scopeFilter).value.flatten }
+  )
+}
