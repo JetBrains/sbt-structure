e38a626a27d8f5b029c65c31753d7394cae2dde0
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2025-02-05 10:49:05 +0100

    minor refactor in test
    This is mostly to create a new commit with the new release tag to test our publishing on CI

--- extractor/src/test/scala/org/jetbrains/sbt/integrationTests/utils/SbtLauncherUtils.scala
+++ extractor/src/test/scala/org/jetbrains/sbt/integrationTests/utils/SbtLauncherUtils.scala
@@ -4,16 +4,15 @@ import java.io.File
 import java.net.URL
 
 object SbtLauncherUtils {
+
+  private val SbtLaunchUrl = "https://repo1.maven.org/maven2/org/scala-sbt/sbt-launch/1.10.7/sbt-launch-1.10.7.jar"
+
   def sbtLauncherPath: String = {
     //isolate the launcher in its own folder (otherwise target from this project can be somehow used)
     val launcher = new File("sbt-launcher/sbt-launch.jar")
 
     if (!launcher.exists())
-      sbt.io.Using.urlInputStream(
-        new URL(
-          "https://repo1.maven.org/maven2/org/scala-sbt/sbt-launch/1.10.7/sbt-launch-1.10.7.jar"
-        )
-      ) { in =>
+      sbt.io.Using.urlInputStream(new URL(SbtLaunchUrl)) { in =>
         sbt.io.IO.transfer(in, launcher)
       }
 
