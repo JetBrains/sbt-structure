b06fa4d532b02bd6e6afe4cb4cdeca9a4d0b5cc3
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-08-24 19:23:31 +0200

    Replace the cacheLevel shim with the @scala.transient annotation which is now accepted by sbt 2.0.0-RC3

--- extractor/src/main/scala/org/jetbrains/sbt/StructureKeys.scala
+++ extractor/src/main/scala/org/jetbrains/sbt/StructureKeys.scala
@@ -2,7 +2,6 @@ package org.jetbrains.sbt
 
 import org.jetbrains.sbt.structure.*
 import sbt.KeyRanks.*
-import sbt.util.cacheLevel
 import sbt.{Configuration, *}
 
 import scala.collection.Seq
@@ -13,22 +12,22 @@ object StructureKeys {
   val dependencyConfigurations: SettingKey[Seq[Configuration]] = SettingKey("ssDependencyConfigurations", description = "", rank = Invisible)
   val sourceConfigurations: SettingKey[Seq[Configuration]] = SettingKey("ssSourceConfigurations", description = "", rank = Invisible)
   val testConfigurations: SettingKey[Seq[Configuration]] = SettingKey("ssTestConfigurations", description = "", rank = Invisible)
-  @cacheLevel(include = Array.empty) val acceptedProjects: TaskKey[Seq[ProjectRef]] = TaskKey("ssAcceptedProjects", description = "", rank = Invisible)
+  @transient val acceptedProjects: TaskKey[Seq[ProjectRef]] = TaskKey("ssAcceptedProjects", description = "", rank = Invisible)
 
-  @cacheLevel(include = Array.empty) val extractPlay2: TaskKey[Option[Play2Data]] = TaskKey("ssExtractPlay2", description = "", rank = Invisible)
-  @cacheLevel(include = Array.empty) val extractBuild: TaskKey[BuildData] = TaskKey("ssExtractBuild", description = "", rank = Invisible)
-  @cacheLevel(include = Array.empty) val extractBuilds: TaskKey[Seq[BuildData]] = TaskKey("ssExtractBuilds", description = "", rank = Invisible)
-  @cacheLevel(include = Array.empty) val extractDependencies: TaskKey[DependencyData] = TaskKey("ssExtractDependencies", description = "", rank = Invisible)
-  @cacheLevel(include = Array.empty) val extractProject: TaskKey[ProjectData] = TaskKey("ssExtractProject", description = "", rank = Invisible)
-  @cacheLevel(include = Array.empty) val extractProjects: TaskKey[Seq[ProjectData]] = TaskKey("ssExtractProjects", description = "", rank = Invisible)
-  @cacheLevel(include = Array.empty) val extractRepository: TaskKey[Option[RepositoryData]] = TaskKey("ssExtractRepository", description = "", rank = Invisible)
-  @cacheLevel(include = Array.empty) val extractStructure: TaskKey[StructureData] = TaskKey("extractStructure", description = "", rank = Invisible)
+  @transient val extractPlay2: TaskKey[Option[Play2Data]] = TaskKey("ssExtractPlay2", description = "", rank = Invisible)
+  @transient val extractBuild: TaskKey[BuildData] = TaskKey("ssExtractBuild", description = "", rank = Invisible)
+  @transient val extractBuilds: TaskKey[Seq[BuildData]] = TaskKey("ssExtractBuilds", description = "", rank = Invisible)
+  @transient val extractDependencies: TaskKey[DependencyData] = TaskKey("ssExtractDependencies", description = "", rank = Invisible)
+  @transient val extractProject: TaskKey[ProjectData] = TaskKey("ssExtractProject", description = "", rank = Invisible)
+  @transient val extractProjects: TaskKey[Seq[ProjectData]] = TaskKey("ssExtractProjects", description = "", rank = Invisible)
+  @transient val extractRepository: TaskKey[Option[RepositoryData]] = TaskKey("ssExtractRepository", description = "", rank = Invisible)
+  @transient val extractStructure: TaskKey[StructureData] = TaskKey("extractStructure", description = "", rank = Invisible)
 
-  @cacheLevel(include = Array.empty) val settingData: TaskKey[Seq[SettingData]] = TaskKey("settingData", description = "", rank = Invisible)
-  @cacheLevel(include = Array.empty) val taskData: TaskKey[Seq[TaskData]] = TaskKey("taskData", description = "", rank = Invisible)
-  @cacheLevel(include = Array.empty) val commandData: TaskKey[Seq[CommandData]] = TaskKey("commandData", description = "", rank = Invisible)
-  @cacheLevel(include = Array.empty) val localCachePath: TaskKey[Option[File]] = TaskKey("localCachePath", description = "", rank = Invisible)
-  @cacheLevel(include = Array.empty) val allKeys: TaskKey[Seq[AttributeKey[_]]] = TaskKey("allKeys", description = "", rank = Invisible)
+  @transient val settingData: TaskKey[Seq[SettingData]] = TaskKey("settingData", description = "", rank = Invisible)
+  @transient val taskData: TaskKey[Seq[TaskData]] = TaskKey("taskData", description = "", rank = Invisible)
+  @transient val commandData: TaskKey[Seq[CommandData]] = TaskKey("commandData", description = "", rank = Invisible)
+  @transient val localCachePath: TaskKey[Option[File]] = TaskKey("localCachePath", description = "", rank = Invisible)
+  @transient val allKeys: TaskKey[Seq[AttributeKey[_]]] = TaskKey("allKeys", description = "", rank = Invisible)
   val allConfigurationsWithSource: SettingKey[Seq[Configuration]] = SettingKey("allConfigurationsWithSource", description = "", rank = Invisible)
 
   val sbtStructureOpts: SettingKey[Options] = SettingKey("ssOptions", "options for dumpStructure task", rank = DSetting)
@@ -37,6 +36,6 @@ object StructureKeys {
   val generateManagedSourcesDuringStructureDump: SettingKey[Boolean] = SettingKey("generateManagedSourcesDuringStructureDump", "generate managed sources during dumpStructure task", rank = DSetting)
 
   val loadOptions: TaskKey[Options] = TaskKey("ssLoadOptions", "load options from ssOptionsFile if available", rank = Invisible)
-  @cacheLevel(include = Array.empty) val dumpStructure: TaskKey[Unit] = TaskKey("dumpStructure", "dump project structure to XML readable by IntelliJ", rank = DTask)
+  @transient val dumpStructure: TaskKey[Unit] = TaskKey("dumpStructure", "dump project structure to XML readable by IntelliJ", rank = DTask)
   val dumpStructureTo: InputKey[File] = InputKey("dumpStructureTo", "dump structure to specified file using provided command line options", rank = DTask)
 }
