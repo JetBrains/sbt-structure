bb03e105281db19c9f820d704d82ca214cc51f5e
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2026-09-25 10:39:04 +0200

    WIP 13

--- extractor/src/main/scala-sbt-1.0-1.x/org/jetbrains/sbt/compat/FileConverterCompat.scala
+++ extractor/src/main/scala-sbt-1.0-1.x/org/jetbrains/sbt/compat/FileConverterCompat.scala
@@ -4,7 +4,7 @@ import sbt.PluginData
 
 /**
  * This class exists to pretend that we have a `xsbti.FileConverter`. It is an interface which exists since sbt 1.4.x.
- * `sbt-structure` compiles against sbt 1.3, so we cannot/don't use it in any of our code.
+ * `sbt-structure` compiles against sbt 1.0 and sbt 1.3, so we cannot/don't use it in any of our code.
  *
  * However, `xsbti.FileConverter` is very important in sbt 2, and the Scala 3/sbt 2 definition of `FileConverterCompat`
  * exposes an underlying instance of `xsbti.FileConverter`.
